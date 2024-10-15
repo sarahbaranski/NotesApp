28b93b9efb13f3b9e5a8b9055ec0c22b4ea4b4fc
sarahbaranski <sbaranski@gmail.com>
2024-10-15 12:55:55 -0500

    successfully connect and create db table

--- main.go
+++ main.go
@@ -16,29 +16,6 @@ const (
 	dbname = "notes_app"
 )
 
-// func main() {
-// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-// 		"dbname=%s sslmode=disable",
-// 		host, port, user, dbname)
-
-// 	db, err := sql.Open("postgres", psqlInfo)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer db.Close()
-
-// 	err = db.Ping()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Println("Successfully connected!")
-// }
-
-// func dsn(dbName string) string {
-// 	return fmt.Sprintf("postgres://%s:password@%s:%d/%s?sslmode=disable", user, host, port, dbName)
-// }
-
 func dbConnection() (*sql.DB, error) {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"dbname=%s sslmode=disable",
@@ -50,54 +27,7 @@ func dbConnection() (*sql.DB, error) {
 		return nil, err
 	}
 
-	// db, err := sql.Open("postgres", dns(dbName))
-	// if err != nil {
-	// 	fmt.Printf("Error %s when opening DB\n", err)
-	// 	return nil, err
-	// }
-
-	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelfunc()
-	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
-	if err != nil {
-		fmt.Printf("Error %s when creating DB\n", err)
-		return nil, err
-	}
-	no, err := res.RowsAffected()
-	if err != nil {
-		fmt.Printf("Error %s when fetching rows", err)
-		return nil, err
-	}
-	fmt.Printf("rows affected: %d\n", no)
-
-	db.Close()
-	db, err = sql.Open("postgres", psqlInfo)
-	if err != nil {
-		fmt.Printf("Error %s when opening DB", err)
-		return nil, err
-	}
-
-	// db, err = sql.Open("postgres", dns(dbName))
-	// if err != nil {
-	// 	fmt.Printf("Error %s when opening DB", err)
-	// 	return nil, err
-	// }
-
-	db.Close()
-	db, err = sql.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s "+
-		"dbname=%s sslmode=disable", host, port, user, dbname))
-	if err != nil {
-		fmt.Printf("Error %s when opening DB", err)
-		return nil, err
-	}
-
-	db.SetMaxOpenConns(20)
-	db.SetMaxIdleConns(20)
-	db.SetConnMaxLifetime(time.Minute * 5)
-
-	ctx, cancelfunc = context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelfunc()
-	err = db.PingContext(ctx)
+	err = db.Ping()
 	if err != nil {
 		fmt.Printf("Errors %s pinging DB", err)
 		return nil, err
